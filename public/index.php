d814b7a9e9066e7de6e7fd6826575335e6b8a000
Blake Simpson <blakesimpson.dev@outlook.com>
2026-09-23 17:15:14 +0800

    feat: wire Doctrine EntityManager into the ServiceManager container
    Also included in this commit: composer now requires `ramsey/uuid` and
    `doctrine/data-fixtures` in preparation for setting up seeding/migration

--- public/index.php
+++ public/index.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-require(dirname(__DIR__) . '/vendor/autoload.php');
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Laminas\Router\Http\TreeRouteStack;
 use Laminas\ServiceManager\ServiceManager;
 use LaminasApiSample\Router;
 
-/** @var TreeRouteStack $route_stack */
-$route_stack = require dirname(__DIR__) . '/config/route_stack.php';
+/** @var TreeRouteStack $routeStack */
+$routeStack = require dirname(__DIR__) . '/config/route-stack.php';
 
-/** @var ServiceManager $service_manager */
-$service_manager = require dirname(__DIR__) . '/config/service_manager.php';
+/** @var ServiceManager $serviceManager */
+$serviceManager = require dirname(__DIR__) . '/config/service-manager.php';
 
-$response = new Router($route_stack, $service_manager)->dispatch();
+$response = new Router($routeStack, $serviceManager)->dispatch();
 $response->send();
