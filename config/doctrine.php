c17e744188cd060484dbb2e5a4fa61007cbc76fe
Blake Simpson <blakesimpson.dev@outlook.com>
2026-09-23 19:20:10 +0800

    fix: use env vars for Postgres config, fix Doctrine connection config

--- config/doctrine.php
+++ config/doctrine.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
+use Doctrine\DBAL\Driver\PDO\PgSQL\Driver;
 use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(
     __DIR__,
-    ['.env', '.env.local'],
+    ['../.env', '../.env.local'],
     false,
     'UTF-8',
 );
@@ -17,12 +18,12 @@ return [
     'doctrine' => [
         'connection' => [
             'orm_default' => [
-                'driver' => 'pdo_pgsql',
+                'driver_class' => Driver::class,
                 'params' => [
                     'host' => 'localhost',
                     'port' => 5432,
-                    'dbname' => 'laminas_api_sample',
-                    'user' => 'laminas_api_sample',
+                    'dbname' => $_ENV['DB_NAME'],
+                    'user' => $_ENV['DB_USER'],
                     'password' => $_ENV['DB_PASSWORD'],
                 ],
             ],
@@ -31,6 +32,22 @@ return [
             'orm_default' => [
                 'class' => AttributeDriver::class,
                 'paths' => [__DIR__ . '/../src'],
+            ],
+        ],
+        'migrations' => [
+            'orm_default' => [
+                'table_storage' => [
+                    'table_name' => 'migrations_executed',
+                    'version_column_name' => 'version',
+                    'version_column_length' => 255,
+                    'executed_at_column_name' => 'executed_at',
+                    'execution_time_column_name' => 'execution_time',
+                ],
+                'migrations_paths' => [
+                    'LaminasApiSample\\Migrations' => 'migrations',
+                ],
+                'all_or_nothing' => true,
+                'check_database_platform' => true,
             ],
         ],
     ],
