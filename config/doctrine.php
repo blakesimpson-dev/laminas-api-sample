18429ac39937c5a9d114bc6499e608fe74c2b5ae
Blake Simpson <blakesimpson.dev@outlook.com>
2026-09-23 17:15:14 +0800

    feat: wire Doctrine EntityManager into the ServiceManager container
    Also included in this commit: composer now requires `ramsey/uuid` and
    `doctrine/data-fixtures` in preparation for setting up seeding/migration

--- config/doctrine.php
+++ config/doctrine.php
@@ -2,7 +2,36 @@
 
 declare(strict_types=1);
 
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(
+    __DIR__,
+    ['.env', '.env.local'],
+    false,
+    'UTF-8',
+);
+$dotenv->load();
+
 return [
     'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driver' => 'pdo_pgsql',
+                'params' => [
+                    'host' => 'localhost',
+                    'port' => 5432,
+                    'dbname' => 'REDACTED',
+                    'user' => 'REDACTED',
+                    'password' => $_ENV['DB_PASSWORD'],
+                ],
+            ],
+        ],
+        'driver' => [
+            'orm_default' => [
+                'class' => AttributeDriver::class,
+                'paths' => [__DIR__ . '/../src'],
+            ],
+        ],
     ],
 ];
