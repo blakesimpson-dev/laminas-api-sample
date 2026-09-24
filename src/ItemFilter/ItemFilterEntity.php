3488b821ca0df1c5b7916c316bb79b387eeba2ea
Blake Simpson <blakesimpson.dev@outlook.com>
2026-09-24 17:26:29 +0800

    revert: restore ItemFilterEntity placeholder until migrations exist

--- src/ItemFilter/ItemFilterEntity.php
+++ src/ItemFilter/ItemFilterEntity.php
@@ -4,32 +4,4 @@ declare(strict_types=1);
 
 namespace LaminasApiSample\ItemFilter;
 
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Embedded;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\Table;
-use LaminasApiSample\Profile\Embedded\TwitchEmbeddable;
-use Ramsey\Uuid\Uuid;
-
-#[Entity(repositoryClass: ItemFilterRepository::class)]
-#[Table(name: 'item_filter')]
-final class ItemFilterEntity
-{
-    #[Column(type: 'guid'), Id]
-    private readonly string $uuid;
-
-    #[Column(name: 'filter_name')]
-    private readonly string $filterName;
-
-    // "realm": "string",
-    // "description": "string",
-    // "version": "string",
-    // "type": "string",
-    // "public": "?bool",
-    // "filter": "?string",
-    // "validation": "?object",
-    // "↳valid": "bool",
-    // "↳version": "?string",
-    // "↳validated": "?string"
-}
+final class ItemFilterEntity {}
