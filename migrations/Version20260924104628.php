6aec0267ec5241260d3a5a9d3d0c237f9113580d
Blake Simpson <blakesimpson.dev@outlook.com>
2026-09-24 18:47:10 +0800

    feat: add item_filter migration

---
+++ migrations/Version20260924104628.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasApiSample\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260924104628 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL120Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL120Platform'."
+        );
+
+        $this->addSql('CREATE TABLE item_filter (id UUID NOT NULL, filter_name VARCHAR(255) NOT NULL, realm VARCHAR(255) NOT NULL, filter TEXT DEFAULT NULL, description VARCHAR(255) NOT NULL, version VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, public BOOLEAN NOT NULL, PRIMARY KEY (id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL120Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL120Platform'."
+        );
+
+        $this->addSql('DROP TABLE item_filter');
+    }
+}
